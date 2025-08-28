ec1a5ead4c252cb91013167fff84aae4828bb1f1
bino1530 <nhatnam1530@gmail.com>
2025-08-28 20:59:42 +0700

    update header chưa có res , thêm tí home do nền trắng

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Header from './components/Header/Header.jsx'
+import Home from './pages/Home/Home.jsx'
 import Footer from './components/Footer/Footer.jsx'
 function App() {
 
   return (
     <>
-      <Footer />
+      <Header />
+      <Home />
+      {/* <Footer /> */}
     </>
   )
 }
