c63e54e2f60853ac4fa9e6f24b70879a2f552d58
bino1530 <nhatnam1530@gmail.com>
2025-08-29 12:51:37 +0700

    routes store

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Header from './components/Header/Header.jsx'
-import Home from './pages/Home/Home.jsx'
 import Footer from './components/Footer/Footer.jsx'
-function App() {
+import Home from './pages/Home/Home.jsx'
+import Store from './pages/Store/Store.jsx'
 
-    return (
-    <BrowserRouter>
+function App() {
+  return (
+    <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/whatsnew" element={<WhatsNew />} />
-        <Route path="/lighting" element={<Lighting />} />
-        <Route path="/furniture" element={<Furniture />} />
-        <Route path="/accessories" element={<Accessories />} />
-        <Route path="/gifts" element={<Gifts />} />
-        <Route path="/explore" element={<Explore />} />
+        <Route path="/store" element={<Store />} />
       </Routes>
-      {/* <Footer /> */}
-    </BrowserRouter>
+      <Footer />
+    </Router>
   )
 }
 
