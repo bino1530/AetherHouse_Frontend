afacb35252ce34c837587ad8d37ab5f0cdd4ebd3
Phuonguyen8758 <dominhnguyen279@gmail.com>
2025-09-06 11:58:43 +0700

    huhui

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/store" element={<Store />} />
         <Route path="/products/:categorySlug?" element={<Products />} />
+        {/* vhg */}
       </Routes>
       <Footer />
     </Router>
