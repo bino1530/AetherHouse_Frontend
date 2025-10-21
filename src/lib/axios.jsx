fe645f4113690f8a652eed9552a56a16979e3c6f
Hoàng Vũ <phamhoangvu281025@gmail.com>
2025-10-21 15:03:21 +0700

    push

--- src/lib/axios.jsx
+++ src/lib/axios.jsx
@@ -8,14 +8,14 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) config.headers.token = `Bearer ${token}`;
   return config;
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.token = `Bearer ${token}`;
     config.headers.token = `Bearer ${token}`;         
   }
   return config;
